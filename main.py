17e328dc853cca5ae67d977341bfe8e20533b397
Carlos <chroliveira.engenharia@gmail.com>
2024-06-18 18:50:24 -0300

    Aulas 8 e 9

--- main.py
+++ main.py
@@ -39,5 +39,78 @@
 
 
 # 13 -Faça um algoritomo que leia o salário e mostre seu novo salário com 15% de aumento
-salario = int(input('Qual o seu salário? '))
-print(f'O valor com o reajuste de 15% fica em R$ {salario+(salario*0.15)}')
+# salario = int(input('Qual o seu salário? '))
+# print(f'O valor com o reajuste de 15% fica em R$ {salario+(salario*0.15)}')
+
+# 4: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+# t = int(input('Digite a temperatura em Celsius: '))
+# f = (t * 1.8) +32
+# print (f'A temperatura em Fahrenheit é de {f}ºF. ')
+
+# 15: Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a
+# quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+# km = float(input('Quantos km foram percorridos? '))
+# dia = int(input('Quantos dias o carro ficou alugado? '))
+
+# calc_km = km * 0.15
+# calc_dia = dia * 60
+
+# print (f'Você ira pagar {calc_km}R$ pelos {km}KM rodados e {calc_dia}R$ pelo o aluguel de {dia} dias')
+
+# 6: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+# import math as mt
+
+# n = float(input('Digite um número decimal: '))
+# print(f'O valor novo é {mt.trunc(n)}')
+
+# 17: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
+# import math as mt
+
+# co = int(input('Digite o valor do cateto oposto '))
+# ca = int(input('Digite o valor do cateto adjacente '))
+
+# hp = mt.hypot(co,ca)
+
+# print (f'O valor da hipotenusa é {hp:.2f}')
+
+# 18Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+# import math as mt
+# ang = int(input('Digite o valor de um ângulo'))
+
+# print (f'O seno desse ângulo é {mt.sin(mt.radians(ang)):.2f}')
+# print (f'O cosseno desse ângulo é {mt.cos(mt.radians(ang)):.2f}')
+# print (f'A tangente desse ângulo é {mt.tan(mt.radians(ang)):.2f}')
+
+# 19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+# Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+# import random as rd
+
+# lista_alunos = ['João', 'Maria', 'Pedro', 'José']
+
+# print (rd.choice(lista_alunos))
+
+# 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+# import random as rd
+
+# lista_alunos = ['João', 'Maria', 'Pedro', 'José']
+# rd.shuffle (lista_alunos)
+
+# print (lista_alunos)
+
+# 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+# N/A
+
+# 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+# – O nome com todas as letras maiúsculas e minúsculas.
+# – Quantas letras ao todo (sem considerar espaços).
+# – Quantas letras tem o primeiro nome.
+
+nome = 'Carlos Oliveira'
+
+print(nome)
+print(len(nome))
+print(len(nome: 6))
