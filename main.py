8675b8e3756f336d53a0d0276f4883757053e6a6
Carlos <chroliveira.engenharia@gmail.com>
2024-06-17 21:10:53 -0300

    Aula guanabara

---
+++ main.py
@@ -0,0 +1,43 @@
+# 06 - monte um valor que receba um número int e de seu sucesso e antecessor
+# numero = int(input ('Digite um número: '))
+# print (f'Sucessor: {numero+1}, Antecessor: {numero-1}')
+
+# 07 - Crie um algoritimo que leia um número e mostre seu dobro, tripo e raiz quadrada
+
+# numero = int(input('Digite um número inteiro '))
+# print (f'Seu dobro é {numero*2} \n Seu triplo é {numero*3} \n Sua raiz quadrada é {numero** (1/2)}')
+
+
+# 08 -Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
+# nota = float(input('Digite a nota do 1ª Prova '))
+# nota_1 = float(input('Digite a nota do 2ª Prova '))
+
+# print (f'A média do aluno é de {(nota+nota_1)/2}')
+
+# 09 - Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+# numero = int(input('Digite um número inteiro '))
+
+# print(f'{numero} x 1 = {numero*1} \n {numero} x 2 = {numero*2} \n {numero} x 3 = {numero*3} \n {numero} x 4 = {numero*4}')
+
+
+# 10 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar 1dol = 3,27
+# d = float(input('Quanto vc tem na carteira em R$? '))
+# print (f'Você pode comprar {(d/3.27):.2f}$ dolls')
+
+# 11 - Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la. 1l tinta = 2m²
+
+# b = float(input('Qual o tamanho da largura da sua parede em metros? '))
+# c = float(input('Qual o tamanho do comprimento da sua parede em metros? '))
+# tinta = (b*c)/2
+# print (f'Sua parede tem {b*c}m², serão necessários {tinta} latas de tinta para pinta-lá')
+
+
+# 12 - Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
+
+# produto = float(input('Qual o preço do produto? '))
+# print (f'O valor com 5% de desconto fica em R$ {produto-(produto*0.05)}')
+
+
+# 13 -Faça um algoritomo que leia o salário e mostre seu novo salário com 15% de aumento
+salario = int(input('Qual o seu salário? '))
+print(f'O valor com o reajuste de 15% fica em R$ {salario+(salario*0.15)}')
